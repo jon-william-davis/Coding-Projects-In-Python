374d8d5ba149bd73bdf3d7344f1fee599f29f985
markdavisdba <markdavisdba@gmail.com>
2018-12-06 21:30:46 -0500

    Day 1 Coding

---
+++ chapter1.py
@@ -0,0 +1,18 @@
+import turtle as t
+
+# pg.18
+# t.forward(200)
+# t.left(90)
+# t.forward(300)
+# print('I am eleven')
+# Keep Turtle window open
+# t.mainloop()
+
+# print(''.join(reversed('Time to code')))
+
+# pg.19
+for counter in range(11):
+    if((counter % 2) == 0):
+        print(str(counter) + ' is even')
+    else:
+        print(str(counter) + ' is odd')
